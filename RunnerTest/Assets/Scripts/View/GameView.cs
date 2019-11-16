25ff7995b167d865e2fa31310cb09e72533262bb
Andrey Godlevskiy <darkday8@bigmir.net>
2019-11-16 18:15:56 +0200

    [+] Textures: ball, building, wall [+] Prefabs: building, wall

--- RunnerTest/Assets/Scripts/View/GameView.cs
+++ RunnerTest/Assets/Scripts/View/GameView.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameView : MonoBehaviour, IView
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Rigidbody player;
+    [SerializeField]
+    private Vector3 distanse;
+
+    private new Camera camera;
+
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player.AddForce(Vector3.right);
+        camera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0) + distanse;
     }
 }
